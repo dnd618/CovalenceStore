37ed078c96c4c0fa6e8673471b28b3b62782d10a
InnovateBham_12 <InnovateBham_12@Admins-iMac.local>
2017-08-15 13:45:09 -0500

    route added

--- client/app.js
+++ client/app.js
@@ -2,8 +2,8 @@ var app = angular.module('myApp', ["ngRoute"]);
 
 app.config(function($routProvider) {
     $routProvider
-    .when("/home", {
-        templateUrl: "../views/home.html",
+    .when("/", {
+        templateUrl: "/views/home.html",
     })
     .run(function($rootScope){
         $rootScope.api = 'http://iambham-store-dev.us-east-1.elasticbeanstalk.com/api/v1/';
