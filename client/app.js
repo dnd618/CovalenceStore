b2248ff29146c4b85b15f523896fee615c0b530e
InnovateBham_12 <InnovateBham_12@Admins-iMac.local>
2017-08-15 13:56:45 -0500

    all routes

--- client/app.js
+++ client/app.js
@@ -5,5 +5,16 @@ app.config(function($routeProvider) {
     .when("/", {
         templateUrl: "../client/views/home.html",
     })
-
+    .when("/apparel", {
+        templateUrl: "../client/views/apparel.html",
+    })
+    .when("/misc", {
+        templateUrl: "../client/views/misc.html",
+    })
+    .when("/checkout", {
+        templateUrl: "../client/views/misc.html",
+    })
+    .when("/invoice", {
+        templateUrl: "../client/views/invoice.html"
+    })
 })
