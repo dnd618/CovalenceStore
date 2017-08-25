c66721a3db43cc8299b56575abda44110683fc33
InnovateBham_13 <InnovateBham_13@Admins-iMac.local>
2017-08-24 21:31:18 -0700

    american express

--- client/app.js
+++ client/app.js
@@ -123,7 +123,7 @@ app.controller("ShoppingController", ['$rootScope', '$scope', '$location', funct
 
 
 // Checkout Controller
-app.controller("CheckoutController", ['$rootScope', '$http', '$scope', '$location', function($rootScope, $http, $scope, $location){
+app.controller("CheckoutController", ['$rootScope', '$http', '$scope', '$location','myFac', function($rootScope, $http, $scope, $location,myFac){
     $rootScope.hideCart = true;
     $rootScope.hideFooter = false;
     var data = {
@@ -141,9 +141,58 @@ app.controller("CheckoutController", ['$rootScope', '$http', '$scope', '$locatio
             $rootScope.total = 0;
         })
     }
-
-
+   
+    var cat = document.getElementById("cscinput");
+    cat.addEventListener("click", function(){
+        var cCard = document.getElementById("cCard").value;
+       myFac.getCardBrand(cCard);
+    });
 }])
+
+app.factory("myFac", function() {
+    var fac = {};
+
+    fac.getCardBrand = function(num) {
+        
+            var brand,
+                patterns = [
+                    { name: 'amex', pattern: /^3[47]/ },
+                    { name: 'dankort', pattern: /^5019/ },
+                    { name: 'dinersclub', pattern: /^(36|38|30[0-5])/ },
+                    { name: 'discover', pattern: /^(6011|65|64[4-9]|622)/ },
+                    { name: 'jcb', pattern: /^(35|1800|2131)/ },
+                    { name: 'laser', pattern: /^(6706|6771|6709)/ },
+                    { name: 'maestro', pattern: /^(5018|5020|5038|6304|6703|6759|676[1-3])/ },
+                    { name: 'mastercard', pattern: /^(5[1-5]|677189)|^(222[1-9]|2[3-6]\d{2}|27[0-1]\d|2720)/ },
+                    { name: 'unionpay', pattern: /^62/ },
+                    { name: 'visaelectron', pattern: /^4(026|17500|405|508|844|91[37])/ },
+                    { name: 'elo', pattern: /^4011|438935|45(1416|76|7393)|50(4175|6699|67|90[4-7])|63(6297|6368)/ },
+                    { name: 'visa', pattern: /^4/ }
+                ];
+    
+            patterns.some(function(p) {
+                if (p.pattern.test(num)) {
+                    brand = p.name;
+                    return true;
+                }
+            });
+            
+            if(brand === "discover"){
+                console.log("This is a discover");
+            } else if(brand === "visa"){
+                console.log("this is a visa");
+                var visa = document.getElementById("visa-img");
+                $(visa).css("border", "4px yellow solid");
+            } else if(brand === "amex"){
+                var amex = document.getElementById("amex");
+                $(amex).css("border", "4px yellow solid")
+                console.log("this is amex");
+            } else if(brand === "mastercard"){
+                console.log("this is a mastercard");
+            }
+    }
+    return fac;
+})
 
 // Invoices Controller
 app.controller("InvoiceController", ['$rootScope', '$http', '$scope', function($rootScope, $http, $scope){
